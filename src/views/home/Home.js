ef588162081cb0db95e5bb10f0d50c7a45ddf8af
Adam Prośniewski <79585504+AdamProsniewski@users.noreply.github.com>
2022-04-26 12:15:31 +0200

    hobbies

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,8 +1,24 @@
 import * as React from 'react';
 import Navbar from '../../components/navbar/Navbar';
+import Card from '../../components/card/Card';
+import Hobbies from '../../data/hobbies.json'
 
 export const Home = () => {
+  const cards = Hobbies.map(item => {
+    return (
+      <Card
+        {...item}
+            
+      />
+    )
+  })
+
   return (
+    <div>
     <Navbar />
+    <section >
+      {cards}
+    </section>
+    </div>
   )
 }
